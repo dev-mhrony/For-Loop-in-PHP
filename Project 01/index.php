8d8997532204fe174ecfefa9e75ab601de6db996
MH-RONY <mh.rony.dhanvi@gmail.com>
2022-04-03 20:03:50 +0600

    For Loop in PHP - 03/04/2022

---
+++ Project 01/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Use For Loop </title>
+</head>
+<body>
+
+    //For loops are used to increase the value by increasing the number one after the other or simply by turning one by one using a formula.
+
+    <?php
+
+    //For example, writing 1 to 100 requires a lot of coding as well as time. So the for loop is made in such a /way that one by one it is taken out or added and its specific value is printed.
+
+    --------------------------------------------------------------------------------------------------------
+
+    //If it is written from 1 to 100, then it will be as it is written below
+
+    --------------------------------------------------------------------------------------------------------
+    
+    // If it is written from 1 to 100, then it will be as it is written below
+
+    // echo 1;
+    // echo 2;
+    // echo 3;
+    // .......
+    // echo 100;
+
+
+
+    //So that requires a lot of time and a lot of code writing. From this we can easily diagnose it using for loop.
+
+
+    
+
+
+    ?>
+
+
+
+    
+</body>
+</html>
